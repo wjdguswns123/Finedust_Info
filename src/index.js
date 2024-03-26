62dd9cfc6b8f0ec95f29c0691d204ef418091d11
wjdguswns123 <archangel123@naver.com>
2024-03-26 19:59:07 +0900

    배포 수정

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const store = createStore(rootReducer);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <App />
       </BrowserRouter>
     </Provider>
